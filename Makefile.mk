f0c1c1e5c90c34f2b513963e1ab1ea4eeb31bed1
archit-10 <architvnt@gmail.com>
2026-01-04 13:27:51 +0530

    Refactor project structure: move files into src/core, src/commands, src/features, src/utilities

--- Makefile.mk
+++ Makefile.mk
@@ -1,10 +1,14 @@
-CXX=g++
-CXXFLAGS=-Wall -pedantic -Werror -g
-SRCS=$(wildcard*.cpp)
-OBJS=$(patsubst %.cpp,%.o,$(SRCS))
+CXX = g++
+CXXFLAGS = -Wall -pedantic -Werror -g
+
+SRCS = $(wildcard src/core/*.cpp src/commands/*.cpp src/features/*.cpp src/utilities/*.cpp)
+OBJS = $(SRCS:.cpp=.o)
 
 %.o: %.cpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $< 
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
 Posix_Shell: $(OBJS)
-	$(CXX) $(CXXFLAGS) -o $@ $^ $(OBJS)
+	$(CXX) $(CXXFLAGS) -o $@ $(OBJS)
+
+clean:
+	rm -f $(OBJS) Posix_Shell
